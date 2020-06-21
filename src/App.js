5817710ac1da27cf2f840194211b91325f9bb5bd
Kevin Cho <kcho@shutterstock.com>
2020-06-21 17:58:09 -0400

    Pull reddit images and render gallery

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
+import axios from 'axios';
+import _ from 'lodash';
+import Gallery from 'react-grid-gallery';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.state = {
+      posts: []
+    };
+  }
+  componentDidMount = async () => {
+    const res = await axios.get('https://www.reddit.com/r/pics.json');
+    const posts = _.get(res, 'data.data.children');
+
+    this.setState({
+      posts
+    });
+  }
+
+  render() {
+    const { posts } = this.state;
+    const galleryImages = posts.reduce((result, post) => {
+      const { url, thumbnail_width: thumbnailWidth, thumbnail_height: thumbnailHeight } = post.data;
+
+      if (url && thumbnailWidth && thumbnailHeight) {
+        result.push({
+          src: url,
+          thumbnail: url,
+          thumbnailWidth,
+          thumbnailHeight
+        });
+      }
+      return result;
+    }, []);
+
+    return (
+      <div className="App">
+        <Gallery images={galleryImages} enableImageSelection={false} rowHeight={300} />
+      </div>
+    );
+  }
+}
